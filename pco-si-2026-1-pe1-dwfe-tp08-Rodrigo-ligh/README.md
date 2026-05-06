005629da7f0992db316307e0fb65ac06b42b48b9
rodrigo <lightlear02@gmail.com>
2026-05-05 22:19:21 -0300

    nome

--- pco-si-2026-1-pe1-dwfe-tp08-Rodrigo-ligh/README.md
+++ pco-si-2026-1-pe1-dwfe-tp08-Rodrigo-ligh/README.md
@@ -5,8 +5,8 @@ Nesta atividade, você fazer exercícios de programação para vai praticar a ma
 
 ## Informações Gerais
 
-- Nome:
-- Matricula:
+- Nome:Rodrigo Dos Santos Libanio
+- Matricula:1110705
 
 ## Prints do console do navegador
 
---
+++ pco-si-2026-1-pe1-dwfe-tp08-Rodrigo-ligh/public/script.js
@@ -0,0 +1,72 @@
+const catalogo = [
+{
+id : 1,
+titulo : "O Poderoso Chefão ",
+tipo : "filme",
+ano :1972,
+generos :["policial"," Drama"],
+nota : 9.2,
+assistido : false,
+},
+
+{
+id : 2,
+titulo : "Um Sonho de Liberdade ",
+tipo : "filme",
+ano :1994,
+generos :"Drama",
+nota : 9.3,
+assistido : false,
+},
+
+{
+id : 3,
+titulo : "Cidadão Kane ",
+tipo : "filme",
+ano :1941,
+generos :["Mistério"," Drama"],
+nota :  8.3,
+assistido : false,
+},
+
+{
+id : 4,
+titulo : "Batman: O Cavaleiro das Trevas ",
+tipo : "filme",
+ano :2008,
+generos :["policial"," Drama","Ação"],
+nota : 9.0,
+assistido : true,
+},
+
+{
+id : 5,
+titulo : "A Lista de Schindler ",
+tipo : "filme",
+ano :1993,
+generos :["História"," Drama","Biografia"],
+nota : 9.0,
+assistido : false,
+},
+
+{
+id : 6,
+titulo : "O Mágico de Oz ",
+tipo : "filme",
+ano :1939,
+generos :[" Fantasia"," Família","Aventura"],
+nota : 8.1,
+assistido : false,
+},
+];
+
+// b.2
+console.log(catalogo);
+console.log("Título do primeiro item", catalogo[0].titulo);
+console.log("Ano do último item:", catalogo[catalogo.length - 1].ano);
+const terceiroFilme = catalogo[2];
+if (terceiroFilme.generos && terceiroFilme.generos.length >= 2) {
+    console.log("Segundo gênero do terceiro item:", terceiroFilme.generos[1]);
+} else {
+    console.log("Somente um gênero");
+}
